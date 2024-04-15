074b0ff294c24deeb81c253ff3ce56a27f1311a7
hyeongseokwoo <woohs0130@naver.com>
2024-04-15 16:00:23 +0900

    feat:neovim/설정 추가

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,6 +4,7 @@
 
 vim.opt.title = true
 vim.opt.autoindent = true
+vim.opt.smartindent = true
 vim.opt.hlsearch = true
 vim.opt.backup = false
 vim.opt.showcmd = true
@@ -15,8 +16,12 @@ vim.opt.shell = "fish"
 vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
 -- vim.opt.inccommand = "nosplit"
 vim.opt.inccommand = "split"
+vim.opt.ignorecase = true
 vim.opt.smarttab = true
 vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.wrap = false -- N
 
 vim.opt.backspace = { "start", "eol", "indent" }
 vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
