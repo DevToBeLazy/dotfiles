db1594d474fb98e0bd3ce372969ec4635059befe
hyeongseok woo <woohs0130@naver.com>
2024-04-19 15:58:13 +0900

    feat:BreakingChange/ end-4 dot file 적용
    https://github.com/end-4/dots-hyprland

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,18 @@
+function fish_prompt -d "Write out the prompt"
+    # This shows up as USER@HOST /home/user/ >, with the directory colored
+    # $USER and $hostname are set by fish, so you can just use them
+    # instead of using `whoami` and `hostname`
+    printf '%s@%s %s%s%s > ' $USER $hostname \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set fish_greeting
 
-    and not set -q TMUX
-    exec tmux
 end
 
 
-set fish_greeting
 set -gx EDITOR nvim
 set force_color_prompt yes
 set -Ux EZA_STANDARD_OPTIONS --icons --hyperlink
@@ -17,9 +23,27 @@ set -g FZF_LEGACY_KEYBINDINGS 0
 
 fish_ssh_agent
 
-starship init fish | source
-zoxide init fish | source
-
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+
+starship init fish | source
+zoxide init fish | source
+
+if test -f ~/.cache/ags/user/generated/terminal/sequences.txt
+    cat ~/.cache/ags/user/generated/terminal/sequences.txt
+end
+
+alias pamcan=pacman
+
+# function fish_prompt
+#   set_color cyan; echo (pwd)
+#   set_color green; echo '> '
+# end
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    and not set -q TMUX
+    exec tmux
+end
