4a63d29116ffd2bef57b2b2ec743b7719e26e0be
DevToBeLazy <woohs0130@naver.com>
2024-04-29 10:43:51 +0900

    style:fish/fzf 색상 catppuccin 적용

--- fish/config.fish
+++ fish/config.fish
@@ -14,12 +14,18 @@ end
 
 
 set -gx EDITOR nvim
-set force_color_prompt yes
+# set force_color_prompt yes
 set -Ux EZA_STANDARD_OPTIONS --icons --hyperlink
 
 # Fzf
 set -g FZF_PREVIEW_FILE_CMD "bat --style=numbers --color=always --line-range :500"
+
 set -g FZF_LEGACY_KEYBINDINGS 0
+
+set -Ux FZF_DEFAULT_OPTS "\
+--color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
+--color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
+--color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796"
 
 fish_ssh_agent
 
@@ -30,7 +36,7 @@ set --export PATH $BUN_INSTALL/bin $PATH
 #cargo
 set --export CARGO_INSTALL "$HOME/.cargo"
 set --export PATH $CARGO_INSTALL/bin $PATH
-
+set --export OBSIDIAN_REST_API_KEY b4029ad8603baa85c40d16c6403e95bc00028fc2bcd2c41ac0a94da0642ae156
 
 starship init fish | source
 zoxide init fish | source
